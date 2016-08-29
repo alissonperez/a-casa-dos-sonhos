56d3dcb44f874d2714370e0958559fbd2df8787c
Alisson Perez <alissonperez@gmail.com>
2016-08-29 11:11:28 -0300

    Add test views for creation and detail of properties

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.urls import reverse
 from django.test import TestCase, Client
 
-from realty import factories
+from realty import factories, models
 from api import serializers
 
 
@@ -54,3 +54,38 @@ class PropertiesTestCase(TestCase):
 
         serializer = serializers.PropertySerializer(prop_soulth)
         self.assertIn(serializer.data, response.data['results'])
+
+    def test_retrieve_must_return_correct_property(self):
+        prop = factories.PropertyFactory(x=1, y=10)
+        url = reverse('property-detail', args=[prop.id])
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        serializer = serializers.PropertySerializer(prop)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_properties_creation(self):
+        prov = factories.ProvinceFactory(
+            x_u=0, y_u=500,
+            x_b=300, y_b=0)
+
+        data = {
+            'x': 222,
+            'y': 444,
+            'title': 'Imóvel código 1, com 5 quartos e 4 banheiros',
+            'price': 1250000,
+            'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            'beds': 4,
+            'baths': 3,
+            'squareMeters': 210
+        }
+
+        self.assertEqual(models.Property.objects.all().count(), 0)
+
+        url = reverse('property-list')
+        response = self.client.post(url, data=data)
+
+        self.assertEqual(response.status_code, 201)
+
+        self.assertEqual(models.Property.objects.all().count(), 1)
